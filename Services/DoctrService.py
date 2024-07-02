60b4cfbfc180464eaa94ce2ed13f519274b1379b
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-02 19:24:56 +0200

    added doctr ocr detection service

---
+++ Services/DoctrService.py
@@ -0,0 +1,22 @@
+class DoctrService:
+
+    @staticmethod
+    def read_in_files(read_in_image):
+        from doctr.io import DocumentFile
+        from doctr.models import ocr_predictor
+
+        model = ocr_predictor(pretrained=True)
+        doc = DocumentFile.from_images(read_in_image)
+        result = model(doc)
+        result = result.export()
+
+        text_list = []
+        # Zugriff auf die "words" in jedem "line" unter "blocks"
+        for page in result['pages']:
+            for block in page['blocks']:
+                for line in block['lines']:
+                    for word in line['words']:
+                        text_list.append(word['value'])
+
+        text_string = ' '.join(text_list)
+        return text_string
