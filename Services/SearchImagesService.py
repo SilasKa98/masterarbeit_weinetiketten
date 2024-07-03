ab8afa6b43a547fbcf9ea800d3fea1d44ec191e9
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-03 20:51:00 +0200

    reworked basic keyword driven search function

--- Services/SearchImagesService.py
+++ Services/SearchImagesService.py
@@ -1,22 +1,45 @@
 from Services.DataProcessService import DataProcessService
 from ActionProcessor import ActionProcessor
+from Services.DatabaseService import DatabaseService
 
 
 class SearchImagesService:
 
-    @staticmethod
-    def search(search_text, use_translation):
+    def __init__(self):
+        self.database_service = DatabaseService()
+
+    def search_normal(self, search_text, used_ocrs=["easyocr", "tesseract", "doctr"]):
         search_text_keywords = DataProcessService.create_keywords_of_scentence(search_text, "de", 4, 6, 0.9)
 
         print(search_text_keywords)
 
-        search_in_directory = ActionProcessor()
-        directory_results = search_in_directory.process_directory("wine_images/archiv20a", use_translation)
-        all_matches = []
-        for image in directory_results:
+        db_results_all = []
+        for ocr in used_ocrs:
+            db_result = self.database_service.select_from_table(ocr, "*", as_dict=True)
+            db_results_all.append(db_result)
+
+        path_text_dict = {}
+        for inner_list in db_results_all:
+            for item in inner_list:
+                for key, value in item.items():
+                    if key == "path":
+                        current_path = value
+                    else:
+                        if current_path in path_text_dict:
+                            path_text_dict[current_path] = path_text_dict[current_path] + " " + value
+                        else:
+                            path_text_dict[current_path] = value
+
+        intersection_dict = {}
+        for key, text in path_text_dict.items():
             for keyword in search_text_keywords:
-                text_intersection = DataProcessService.find_text_intersections(keyword[0], image[0], image[1])
+                text_intersection = DataProcessService.find_text_intersections(keyword[0], text)
                 if text_intersection is not None:
-                    all_matches.append([text_intersection])
-        return all_matches
+                    text_intersection_str = next(iter(text_intersection))
+                    if text_intersection_str in intersection_dict:
+                        intersection_dict[text_intersection_str].add(key)
+                    else:
+                        intersection_dict[text_intersection_str] = {key}
+
+        return intersection_dict
 
--- playground.py
+++ playground.py
@@ -1,7 +1,14 @@
 import json
 
 from Services.DoctrService import DoctrService
+from Services.SearchImagesService import SearchImagesService
+
+#doctr = DoctrService()
+#data = doctr.read_in_files_with_kie("C:\\Masterarbeit_ocr_env\\wine_images\\uploads\\i00234a01.jpg")
 
 
-doctr = DoctrService()
-data = doctr.read_in_files_with_kie("C:\\Masterarbeit_ocr_env\\wine_images\\uploads\\i00234a01.jpg")
+search = SearchImagesService()
+test = search.search_normal("Zeige mir weine aus italien")
+
+print(test)
+print(list(test.keys()))
