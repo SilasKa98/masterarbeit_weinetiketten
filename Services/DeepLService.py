512bb9a8208ae9339acf7cdc5ec764e96d36a9d0
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-09 15:10:24 +0200

    improved deepl language detection
    added force_update function to update all languages regardless of their existenze in the database

--- Services/DeepLService.py
+++ Services/DeepLService.py
@@ -27,10 +27,24 @@ class DeepLService:
         except KeyError:
             return "en"
 
-    def detect_language(self, input_text, image_name, image_directory_name):
+    def detect_language(self, input_text, image_name, image_directory_name, force_update=False):
 
         #use keywords of the full context to detect the language to not use too many words for deepl (500k/month free)
-        keyword_search = DataProcessService.create_keywords_of_scentence(input_text, "en", 1 , 5, 0.9)
+        keyword_search = DataProcessService.create_keywords_of_scentence(input_text, "en", 1, 7, 0.9)
+
+        #get keyword text. If its not formed by create_keywords_of_scentence, it will be picking middle 5 words of text
+        if len(keyword_search) > 0:
+            keyword_text = keyword_search[0][0]
+        else:
+            text_splitted = input_text.split()
+            num_words = len(text_splitted)
+            if num_words <= 5:
+                keyword_text = ' '.join(text_splitted)
+            else:
+                middle_index = num_words // 2
+                start_index = max(0, middle_index - 2)
+                end_index = min(num_words, start_index + 5)
+                keyword_text = ' '.join(text_splitted[start_index:end_index])
 
         try:
             #first search in db for the specific image if there already is a translation
@@ -42,8 +56,8 @@ class DeepLService:
                 [image_name,image_directory_name]
             )
             found_language_in_db = database_entry[0][0]
-            if found_language_in_db == "N/A" or found_language_in_db == "unknown" or found_language_in_db == "" or found_language_in_db is None:
-                result = self.translator.translate_text(keyword_search[0][0], target_lang="EN-GB")
+            if found_language_in_db == "N/A" or found_language_in_db == "unknown" or found_language_in_db == "" or found_language_in_db is None or force_update is True:
+                result = self.translator.translate_text(keyword_text, target_lang="EN-GB")
                 deepl_detection = result.detected_source_lang
                 return deepl_detection
             else:
