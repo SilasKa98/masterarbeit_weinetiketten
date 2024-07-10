0cac291674befdc670b3a06fe087bb3af55a9551
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-10 20:26:18 +0200

    testing

--- playground.py
+++ playground.py
@@ -1,15 +1,18 @@
 from Services.DataProcessService import DataProcessService
 from Services.DetailFinderService import DetailFinderService
 from ActionProcessor import ActionProcessor
+from Services.SpellcheckerService import SpellcheckerService
 
-data = DataProcessService()
+
 #loaded_input_file = preprocessor.load_data('data/english_words.csv')
 #print(loaded_input_file)
-'''
-filepath = "C:\\Users\\Silas-Pc\\Downloads\\dewiki-latest-pages-articles\\dewiki-latest-pages-articles.xml"
-output_file = "dictionary_files/german_extracted_words_20mio_uml.txt"
-data.create_txt_from_wikimedia(filepath, output_file, 20000000)
-'''
+
+data = DataProcessService()
+
+filepath = "D:\\masterarbeit_saver_all\\frwiki-latest-pages-articles.xml\\frwiki-latest-pages-articles.xml"
+output_file = "dictionary_files/french_extracted_words_750k_uml_fr.txt"
+data.create_txt_from_wikimedia(filepath, output_file, 750000, language="fr")
+
 
 '''
 details = DetailFinderService()
@@ -22,7 +25,7 @@ print(countries)
 provinces = details.find_provinces("wine_images\\archiv20a\\merl_koenigslay_terrassen.jpg")
 print(provinces)
 '''
-action = ActionProcessor()
+#action = ActionProcessor()
 
-all_paths_in_db_tupels = action.update_label_detail_infos()
+#all_paths_in_db_tupels = action.update_label_detail_infos()
 
