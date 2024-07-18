09bbb9d6b72835d4e4d8cbf8f8f452ba463c75a8
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-18 21:11:53 +0200

    added backend for other handlers

--- Services/InterfaceService.py
+++ Services/InterfaceService.py
@@ -16,6 +16,18 @@ class InterfaceService:
         self.app.run(debug=True)
 
     def handlers(self):
+
+        @self.app.route('/status', methods=['GET'])
+        def get_all_task_status():
+            return jsonify(self.tasks)
+
+        @self.app.route('/status/<task_name>', methods=['GET'])
+        def get_task_status(task_name):
+            if task_name in self.tasks:
+                return jsonify(self.tasks[task_name])
+            else:
+                return jsonify({"status": "unknown", "name": "unknown"})
+
         @self.app.route('/test', methods=['POST'])
         def read_ocr():
             data = request.json
@@ -45,7 +57,7 @@ class InterfaceService:
             threading.Thread(target=self.process_spelling_correction,
                              args=(task_id, task_name, table, sel_columns, insert_column, use_ml, lang_filter)).start()
 
-            self.tasks[task_id] = {
+            self.tasks[task_name] = {
                 "task_id": task_id,
                 "status": "processing",
                 "name": task_name
@@ -58,23 +70,95 @@ class InterfaceService:
             }
             return jsonify(response)
 
-        @self.app.route('/status', methods=['GET'])
-        def get_all_task_status():
-            return jsonify(self.tasks)
+        @self.app.route('/update_label_detail_infos', methods=['POST'])
+        def update_label_detail_infos():
 
-        @self.app.route('/status/<task_id>', methods=['GET'])
-        def get_task_status(task_id):
-            if task_id in self.tasks:
-                return jsonify(self.tasks[task_id])
-            else:
-                return jsonify({"status": "unknown", "name": "unknown"})
+            task_id = str(uuid.uuid4())
+            task_name = "update_label_detail_infos"
 
-    # function to async handle the processing
+            threading.Thread(target=self.process_update_label_detail_infos, args=(task_id, task_name)).start()
+
+            self.tasks[task_name] = {
+                "task_id": task_id,
+                "status": "processing",
+                "name": task_name
+            }
+
+            response = {
+                "task_id": task_id,
+                "status": "processing",
+                "name": task_name
+            }
+            return jsonify(response)
+
+        @self.app.route('/search_for_duplicate_entrys', methods=['POST'])
+        def search_for_duplicate_entrys():
+            data = request.json
+            table = data["table"]
+            column = data["column"]
+            save = data["save"]
+            save_table = "duplicates"
+            task_id = str(uuid.uuid4())
+            task_name = "search_for_duplicate_entrys"
+
+            threading.Thread(target=self.process_search_for_duplicate_entrys,
+                             args=(task_id, task_name, table, column, save, save_table)).start()
+
+            self.tasks[task_name] = {
+                "task_id": task_id,
+                "status": "processing",
+                "name": task_name
+            }
+
+            response = {
+                "task_id": task_id,
+                "status": "processing",
+                "name": task_name
+            }
+            return jsonify(response)
+
+    # ------------------------------------------------Processing--------------------------------------------------------
+    # functions to async handle the processing
     def process_spelling_correction(self, task_id, task_name, table, sel_columns, insert_column, use_ml, lang_filter):
 
         from ActionProcessor import ActionProcessor
         action_processor = ActionProcessor()
         action_processor.correct_sentence_spelling(table, sel_columns, insert_column, use_ml=use_ml, lang_filter=lang_filter)
 
-        self.responses[task_id] = {"status": "success",
-                                   "name": task_name}
+        self.tasks[task_name] = {"status": "success",
+                                 "name": task_name,
+                                 "task_id": task_id
+                                 }
+
+    def process_update_label_detail_infos(self, task_id, task_name):
+        from ActionProcessor import ActionProcessor
+        action_processor = ActionProcessor()
+        action_processor.update_label_detail_infos()
+
+        self.tasks[task_name] = {"status": "success",
+                                 "name": task_name,
+                                 "task_id": task_id
+                                 }
+
+    def process_search_for_duplicate_entrys(self, task_id, task_name, table, column, save, save_table):
+
+        from ActionProcessor import ActionProcessor
+        action_processor = ActionProcessor()
+        similarity_result = action_processor.search_for_duplicate_entrys(table, column, save=save, save_table=save_table)
+
+        return_strings = []
+        for item in similarity_result:
+            # extract paths out of the tupels
+            path1 = item[3][0] if isinstance(item[3], tuple) else item[3]
+            path2 = item[4][0] if isinstance(item[4], tuple) else item[4]
+
+            append_string = f"<p>Ratio: {item[2]} | Paths: <a href=/{path1} target='_blank'>{path1}</a> & <a href=/{path2} target='_blank'>{path2}</a></p>"
+            return_strings.append(append_string)
+
+        self.tasks[task_name] = {"status": "success",
+                                 "name": task_name,
+                                 "task_id": task_id,
+                                 "result": return_strings
+                                 }
+
+
