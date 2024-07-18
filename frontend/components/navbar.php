3146d25edba3d0826391547a86e1d4734cceea79
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-18 14:46:47 +0200

    added navbar and footer component

---
+++ frontend/components/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-md bg-dark sticky-top border-bottom" data-bs-theme="dark">
+    <div class="container">
+        <div class="offcanvas-body">
+            <ul class="navbar-nav flex-grow-1 justify-content-between">
+                <li class="nav-item"><a class="nav-link" href="admin_page.php">Admin</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">foo</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">bar</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">buz</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">test</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">bla</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
