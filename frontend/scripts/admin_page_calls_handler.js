5e760e333ada6b6fe0d14e54bd20087d41fec6e8
SilasKa98 <silas.kammerer@googlemail.com>
2024-07-25 20:57:38 +0200

    renamed

--- frontend/scripts/admin_page_calls_handler.js
+++ frontend/scripts/admin_page_calls_handler.js
@@ -252,47 +252,4 @@ function run_read_db_and_detect_lang(){
 }
 
 
-function status_polling(){
-    $.ajax({
-        type: "GET",
-        url: "http://127.0.0.1:5000/status",
-        contentType: 'application/json',
-        success: function(response){ 
-            console.log(response)
-            Object.keys(response).forEach(function(key) {
-                console.log(response[key]["name"])
-                let task_name = response[key]["name"]
-                var task_status = response[key]["status"]
-                var spinner_elem = document.getElementById("spinner_"+task_name)
-                var success_elem = document.getElementById("success_"+task_name)
-                if(task_status == "processing"){
-                    success_elem.style.display = "none";
-                    spinner_elem.style.display = "block";
-                }else if(task_status == "success"){
-                    spinner_elem.style.display = "none";
-                    success_elem.style.display = "block";
-                }else{
-                    spinner_elem.style.display = "none";
-                    success_elem.style.display = "none";
-                }
 
-                if(response[key]["result"]){
-                    console.log(response[key]["result"])
-                    var result_elem = document.getElementById("result_"+task_name)
-
-
-                    if(task_name == "search_for_duplicate_entrys"){
-                        document.getElementById("result_"+task_name+"_btn").style.display = "block"
-                        result_elem.innerHTML = ""
-                        response[key]["result"].forEach(resElem => {
-                            result_elem.innerHTML += resElem+"<br>"
-                        });
-                    }
-                }
-            });
-        }
-    });
-
-
-    setTimeout(status_polling, 15000);
-}
