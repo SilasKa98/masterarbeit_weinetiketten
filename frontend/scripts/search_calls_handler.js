c3cd38f1a42dd79a178737d811e350527aa4c72b
SilasKa98 <silas.kammerer@googlemail.com>
2024-08-01 23:28:10 +0200

    adjusted script handler for coherent search

--- frontend/scripts/search_calls_handler.js
+++ frontend/scripts/search_calls_handler.js
@@ -1,11 +1,18 @@
 function process_search(){
-    search_text = document.getElementById("search_text").value
+    let search_text = document.getElementById("search_text").value
+    let search_logic_combined = document.getElementById("search_logic_combined")
+    if(search_logic_combined.checked){
+        search_logic_combined = true
+    }else{
+        search_logic_combined = false
+    }
     $.ajax({
         type: "POST",
         url: "http://127.0.0.1:5000/search_algorithm",
         contentType: 'application/json',
         data: JSON.stringify({
-            search_text: search_text
+            search_text: search_text,
+            search_logic_combined: search_logic_combined
         }),
         success: function(response){ 
             console.log(response)
