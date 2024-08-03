2167c6d407331344ef509d24f385da984c0e0da1
SilasKa98 <silas.kammerer@googlemail.com>
2024-08-03 23:11:23 +0200

    testing

--- playground.py
+++ playground.py
@@ -14,9 +14,9 @@ from Services.SearchImagesService import SearchImagesService
 #output_file = "dictionary_files/english_extracted_words_750k_uml_en.txt"
 #data.create_txt_from_wikimedia(filepath, output_file, 750000, language="en")
 
-action_processor3 = ActionProcessor()
-action_processor3.correct_sentence_spelling("tesseract", "text_translation_modified_images, tesseract.path",
-                                            "text_final", use_ml=True)
+#action_processor3 = ActionProcessor()
+#action_processor3.correct_sentence_spelling("tesseract", "text_translation_modified_images, tesseract.path",
+                                          #  "text_final", use_ml=True)
 
 
 #action = ActionProcessor()
@@ -28,8 +28,8 @@ action_processor3.correct_sentence_spelling("tesseract", "text_translation_modif
 #entities = search.named_entity_recognition("Zeige mir Riesling und Merlot Weine aus der Region Mosel von 1900")
 #print(entities)
 
-'''
-details = DetailFinderService()
+
+details = DetailFinderService(init_path_text_dict=True)
 vol = details.find_vol("wine_images\\archiv20a\\merl_koenigslay_terrassen.jpg")
 print(vol)
 anno = details.find_anno("wine_images\\archiv20a\\merl_koenigslay_terrassen.jpg")
@@ -38,7 +38,7 @@ countries = details.find_country("wine_images\\archiv20a\\merl_koenigslay_terras
 print(countries)
 provinces = details.find_provinces("wine_images\\archiv20a\\merl_koenigslay_terrassen.jpg")
 print(provinces)
-'''
+
 #action = ActionProcessor()
 
 #all_paths_in_db_tupels = action.update_label_detail_infos()
