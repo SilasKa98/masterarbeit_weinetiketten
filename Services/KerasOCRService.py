8b6f396697c9a974c45ffdd91d5005c079826cb7
SilasKa98 <silas.kammerer@googlemail.com>
2024-08-15 15:34:55 +0200

    added try except logic for kerasocr to catch errors while reading

--- Services/KerasOCRService.py
+++ Services/KerasOCRService.py
@@ -7,14 +7,18 @@ class KerasOCRService:
         self.pipeline = keras_ocr.pipeline.Pipeline()
 
     def read_in_files(self, read_in_image):
-        images = [keras_ocr.tools.read(url) for url in [read_in_image]]
-        prediction_groups = self.pipeline.recognize(images)
+        try:
+            images = [keras_ocr.tools.read(url) for url in [read_in_image]]
+            prediction_groups = self.pipeline.recognize(images)
 
-        text_list = []
-        for item in prediction_groups:
-            for inner_item in item:
-                text_list.append(inner_item[0])
+            text_list = []
+            for item in prediction_groups:
+                for inner_item in item:
+                    text_list.append(inner_item[0])
 
-        text_string = " ".join(text_list)
-        return text_string
+            text_string = " ".join(text_list)
+            return text_string
+        except Exception as e:
+            print(f"Error processing image {read_in_image}: {str(e)}")
+            return "error"
 
