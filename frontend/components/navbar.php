ec19ff51ae1400859933eb9e4898827b67a21d36
SilasKa98 <silas.kammerer@googlemail.com>
2024-08-18 22:51:21 +0200

    small visual fix to the navbar

--- frontend/components/navbar.php
+++ frontend/components/navbar.php
@@ -2,7 +2,7 @@
 <a href="/frontend/index.php"><img src="/frontend/media/wine_logo_white.png" alt="our logo" width="125"></a>
     <div class="container">
         <div class="offcanvas-body">
-            <ul class="navbar-nav flex-grow-1 justify-content-between">
+            <ul class="navbar-nav flex-grow-1 justify-content">
                 <li class="nav-item"><a class="nav-link" href="" data-bs-toggle='offcanvas' data-bs-target='#offcanvas_imgDetails' aria-controls='offcanvas_imgDetails'>Etikettendetails</a></li>
                 <li class="nav-item"><a class="nav-link" href="admin_page.php">Admin</a></li>
             </ul>
