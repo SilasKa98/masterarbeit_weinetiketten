b0c77bcedebf4c0ce6b82797f32496e6642d3781
SilasKa98 <silas.kammerer@googlemail.com>
2024-08-18 16:08:34 +0200

    improved image detail info polling

--- frontend/scripts/conent_filling_handler.js
+++ frontend/scripts/conent_filling_handler.js
@@ -18,63 +18,71 @@ function handel_offcanvas_content_filling(path){
                 }
             }
             specificTaskStatusPolling(taskName).then(response => {
-                console.log(response);
-                console.log(response.status);
-                console.log(response.result);
-                var result = response.result
 
-                // pre processing and filtering
-                if(result["image_anno"]){
-                    result["image_anno"] = result["image_anno"] || "";
-                }
-                const wineTypes = {
-                    "white wine": "Weißwein",
-                    "red wine": "Rotwein",
-                    "unclear": ""
-                };
-                result["image_wine_type"] = wineTypes[result["image_wine_type"]] ?? result["image_wine_type"];
-       
-                let labels = [
-                    { key: "image_directory", label: "Etiketten-Verzeichnis" },
-                    { key: "image_lang", label: "Etikettensprache" },
-                    { key: "image_country", label: "Herkunftsland" },
-                    { key: "image_provinces", label: "Region" },
-                    { key: "image_anno", label: "Jahrgang" },
-                    { key: "image_vol", label: "Alkoholgehalt" },
-                    { key: "image_wine_type", label: "Weinsorte" },
-                    { key: "wine_name", label: "Weinname/Rebsorte"},
-                    { key: "google_query", label: "Google" }
-                ];
+                if (response.status === "processing") {
+                    setTimeout(() => {
+                        handel_offcanvas_content_filling(path);
+                    }, 500);
+                }else{
 
-                let content = '';
+                    console.log(response);
+                    console.log(response.status);
+                    console.log(response.result);
+                    var result = response.result
 
-                labels.forEach(item => {
-                    if (result[item.key]) {
-                        if(item.key == "google_query"){
-                            let formattedQuery = result[item.key].replace(/ /g, '+')
-                            let encodedQuery = encodeURIComponent(formattedQuery).replace(/%2B/g, '+')
-                            content += `<br><span class="offCanvasCardLabel"></span><a href=https://www.google.com/search?q=${encodedQuery}&tbm=shop target='_blank'>Ähnliche Weine auf Google suchen</a><br>`;
-                        }else{
-                            content += `<span class="offCanvasCardLabel">${item.label}: </span><span>${result[item.key]}</span><br>`;
-                        }
-                        
+                    // pre processing and filtering
+                    if(result.hasOwnProperty("image_anno")){
+                        result["image_anno"] = result["image_anno"] || "";
                     }
-                });
+                    const wineTypes = {
+                        "white wine": "Weißwein",
+                        "red wine": "Rotwein",
+                        "unclear": ""
+                    };
 
-                let contentString = '<button type="button" onclick="removeElement(this)" class="btn btn-danger delDetailImgBtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path></svg></button>'+
-                                    '<div class="card mb-3 offCanvasCard">'+
-                                        '<a href="../'+ path +'" target="_blank"><img src="../' + path + '" class="card-img-top offCanvasCardImg" alt="Etikettenbild"></a>'+
-                                        '<div class="card-body">'+
-                                            '<h5 class="card-title">'+result["image_name"]+'</h5>'+
-                                            '<p class="card-text">'+
-                                                content + 
-                                            '</p>'+
-                                            '<p class="card-text"><small class="text-body-secondary">Last updated just now</small></p>'+
-                                        '</div>'+
-                                      '</div>';
+                    result["image_wine_type"] = wineTypes[result["image_wine_type"]] ?? result["image_wine_type"];
+        
+                    let labels = [
+                        { key: "image_directory", label: "Etiketten-Verzeichnis" },
+                        { key: "image_lang", label: "Etikettensprache" },
+                        { key: "image_country", label: "Herkunftsland" },
+                        { key: "image_provinces", label: "Region" },
+                        { key: "image_anno", label: "Jahrgang" },
+                        { key: "image_vol", label: "Alkoholgehalt" },
+                        { key: "image_wine_type", label: "Weinsorte" },
+                        { key: "wine_name", label: "Weinname/Rebsorte"},
+                        { key: "google_query", label: "Google" }
+                    ];
 
-                document.getElementById("offcanvas_imgDetails_body").insertAdjacentHTML("afterbegin", contentString);
-                
+                    let content = '';
+
+                    labels.forEach(item => {
+                        if (result[item.key]) {
+                            if(item.key == "google_query"){
+                                let formattedQuery = result[item.key].replace(/ /g, '+')
+                                let encodedQuery = encodeURIComponent(formattedQuery).replace(/%2B/g, '+')
+                                content += `<br><span class="offCanvasCardLabel"></span><a href=https://www.google.com/search?q=${encodedQuery}&tbm=shop target='_blank'>Ähnliche Weine auf Google suchen</a><br>`;
+                            }else{
+                                content += `<span class="offCanvasCardLabel">${item.label}: </span><span>${result[item.key]}</span><br>`;
+                            }
+                            
+                        }
+                    });
+
+                    let contentString = '<button type="button" onclick="removeElement(this)" class="btn btn-danger delDetailImgBtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path></svg></button>'+
+                                        '<div class="card mb-3 offCanvasCard">'+
+                                            '<a href="../'+ path +'" target="_blank"><img src="../' + path + '" class="card-img-top offCanvasCardImg" alt="Etikettenbild"></a>'+
+                                            '<div class="card-body">'+
+                                                '<h5 class="card-title">'+result["image_name"]+'</h5>'+
+                                                '<p class="card-text">'+
+                                                    content + 
+                                                '</p>'+
+                                                '<p class="card-text"><small class="text-body-secondary">Last updated just now</small></p>'+
+                                            '</div>'+
+                                        '</div>';
+
+                    document.getElementById("offcanvas_imgDetails_body").insertAdjacentHTML("afterbegin", contentString);
+                }
 
             }).catch(error => {
                 console.error("Error:", error);
